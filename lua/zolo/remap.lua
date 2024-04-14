e0fae6db72b5d295fde14a97770275fb5b9fd52d
Zoltan Nemes <nemesz2000@gmail.com>
2024-04-14 12:56:44 +0200

    Adding some remaps

--- lua/zolo/remap.lua
+++ lua/zolo/remap.lua
@@ -1,6 +1,18 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<c-j>", vim.cmd.cnext)
-vim.keymap.set("n", "<c-k>", vim.cmd.cprev)
 
+vim.keymap.set("n", "<c-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<c-k>",  "<cmd>cnext<CR>zz")
 
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "<leader>wr", "<cmd>vertical resize 106<CR>")

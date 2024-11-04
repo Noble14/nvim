852c86dcb2a8b646bfc03473c894cfcd50c4003b
Zoltan Nemes <zoltan.nemes@ericsson.com>
2024-11-04 11:09:03 +0100

    [nvim] change harpoon keybindings

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,16 +1,31 @@
 local harpoon = require("harpoon")
 
 -- REQUIRED
-harpoon:setup()
+harpoon:setup({
+      settings = {
+        save_on_toggle = false,
+
+        -- save harpoon list to git branch
+        key = function()
+          local handle = io.popen("git branch --show-current 2> /dev/null")
+          local git_branch = handle:read("*a") or ""
+          handle:close()
+          git_branch = string.gsub(git_branch, "\n", "")
+          local cwd = vim.fn.getcwd()
+
+          return git_branch .. cwd
+        end,
+      },
+    })
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<M-j>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<M-k>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<M-l>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<M-h>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)

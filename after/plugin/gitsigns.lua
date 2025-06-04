e7231bf896c60f7c1fc03b9b19aa6a40be883726
Zoltan Nemes <nemesz2000@gmail.com>
2025-06-04 13:40:39 +0200

    gitsigns: remove commented code

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,19 +1,3 @@
---require('gitsigns').setup{
---    map('n', '<leader>hs', gitsigns.stage_hunk)
---    map('n', '<leader>hr', gitsigns.reset_hunk)
---    map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
---    map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
---    map('n', '<leader>hS', gitsigns.stage_buffer)
---    map('n', '<leader>hu', gitsigns.undo_stage_hunk)
---    map('n', '<leader>hR', gitsigns.reset_buffer)
---    map('n', '<leader>hp', gitsigns.preview_hunk)
---    map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
---    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
---    map('n', '<leader>hd', gitsigns.diffthis)
---    map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
---    map('n', '<leader>td', gitsigns.toggle_deleted)
---end
---}
 require('gitsigns').setup{
   on_attach = function(bufnr)
     local gitsigns = require('gitsigns')
